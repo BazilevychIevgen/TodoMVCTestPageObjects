858583acfe701f7d20cf0ae45073c7d995febe46
Ievgen <ievgenbazilevych@gmail.com>
2016-07-29 22:58:33 -0400

    Hi

---
+++ src/test/java/feature/test/BaseTest.java
@@ -0,0 +1,29 @@
+package feature.test;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Screenshots;
+import com.google.common.io.Files;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import ru.yandex.qatools.allure.annotations.Attachment;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by barocko on 7/29/2016.
+ */
+public class BaseTest {
+
+    @After
+    public void tearDown() throws IOException {
+        screenshot();
+    }
+
+    @Attachment(type = "image/png")
+    public byte[] screenshot() throws IOException {
+        File screenshot = Screenshots.takeScreenShotAsFile();
+        return Files.toByteArray(screenshot);
+    }
+}
